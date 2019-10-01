00faa1b98859e342fa11631eab0e10409fbb749b
ruslan <ruslan.a@openlegacy.com>
2019-10-01 15:40:56 +0300

    adding chmod

---
+++ chmod.cpp
@@ -0,0 +1,38 @@
+//
+// Created by rusla on 10/1/2019.
+//
+
+#include <sys/types.h>
+#include <sys/dir.h>
+#include <sys/param.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#include <string>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    string str;
+
+    if (argc != 3) {
+        printf("Usage: %s [permission oct representation] [ file dir ] \n", argv[0]);
+        exit(1);
+    }
+
+    if (strlen(argv[1]) != 3) {
+        printf("Please set a 3 number permission representation\n");
+        exit(1);
+    }
+    str = argv[1];
+    for(char& c : str) {
+        if (7 - c < 0 || 7 - c > 7){
+            printf("Please set a value between 0 to 7\n");
+            exit(1);
+        }
+    }
+
+    str.insert(0, "0");
+    chown(argv[2], str);
+
+}
